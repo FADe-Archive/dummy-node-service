f5b1ad2d7009b7fe2fae1fb36da1b186b87b15bd
ldmsys <16889349+ldmsys@users.noreply.github.com>
2020-09-02 13:09:19 +0900

    add: Content

---
+++ main.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const process = require('process');
+let interval = 1000;
+
+function info(sig) {
+    console.log(`Got ${sig}`);
+    process.exit(1);
+}
+process.on('SIGINT', info);
+process.on('SIGTSTP', info);
+process.on('SIGBREAK', info);
+process.on('SIGQUIT', info);
+
+console.log(`Dummy Node Service - Started on ${new Date()}`)
+console.log(`Will tick every ${interval/1000} seconds (${interval}ms), Customize with argument (${process.argv[1]} [ms])`)
+
+setInterval(() => {
+    console.log("Tick!");
+}, interval);
